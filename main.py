4306604a12b299747dfa1ee699a4b4618897e502
radimkocko <159121941+radimkocko@users.noreply.github.com>
2024-11-20 10:24:10 +0100

    Dokonceno

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def bubble_sort():
     n = len(array)
     for i in range(n-1):
         for j in range(0, n-i-1):
-            if array[j] > array[j+1] :
+            if array[j] > array[j+1]:
                array[j], array[j+1] = array[j+1], array[j]
             print(array)
     return array
